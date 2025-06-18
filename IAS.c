9e8b53db7c80ace7738c312207aa7a2d5d6cb2f8
JyCxX <juliomelo3463@gmail.com>
2025-06-18 15:02:45 -0300

    corrige logica para alunos acima 8

--- IAS.c
+++ IAS.c
@@ -1,16 +1,21 @@
 
+
+#include <stdio.h>
+
 int main(void) {
     float nota1,nota2,media;
     printf("Qual a primeira nota?");
-    scanf("%f",&nota1); 
+    scanf("%f",&nota1);
     printf("Qual a segunda nota?");
-    scanf("%f",&nota2); 
+    scanf("%f",&nota2);
     media = (nota1+nota2)/2;
-    if(media>=6){
-    printf("media : %.2f aprovado", media);
-    } else if(media>=9){
-    printf(" se inscreva para a monitoria!!!!");
-    } else
-    printf("media : %.2f te convido a estudar", media);
+    // LÓGICA CORRIGIDA
+    if(media>=9){
+        printf("media : %.2f, parabens! Se inscreva para a monitoria!!!!", media);
+    } else if(media>=6){
+        printf("media : %.2f, aprovado", media);
+    } else {
+        printf("media : %.2f, te convido a estudar mais", media);
+    }
     return 0;
-}
+
